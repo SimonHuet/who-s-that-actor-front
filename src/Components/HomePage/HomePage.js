ddff2055c0eda7df723c938d1e5ba5ce354bc7a2
SimonHuet <huetsimon35@gmail.com>
2021-03-05 08:30:00 +0100

    feat: set front nav and footer

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,10 +1,8 @@
-import { Banner, Footer, Nav } from '../UI'
+import { Banner } from '../UI'
 import './HomePage.css'
 
-const children = [
-  <Nav id="Nav" key="Nav" />,
-  <Banner id="Banner" key="Banner" />,
-  <Footer id="Footer" key="Footer" />,
-]
-
-export const HomePage = () => <div className="wrapper">{children}</div>
+export const HomePage = () => (
+  <div className="wrapper">
+    <Banner id="Banner" key="Banner" />
+  </div>
+)
