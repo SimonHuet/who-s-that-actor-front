c27c36295b595726fc790e7b3ca8f3297022629a
SimonHuet <huetsimon35@gmail.com>
2021-03-07 20:41:52 +0100

    fix: remove useless reducers

--- src/State/Record.js
+++ src/State/Record.js
@@ -2,16 +2,6 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const INITIAL_STATE = {
   error: null,
-  audioDetails: {
-    url: null,
-    blob: null,
-    chunks: null,
-    duration: {
-      h: 0,
-      m: 0,
-      s: 0,
-    },
-  },
   prediction: null,
 }
 
@@ -20,11 +10,6 @@ const slice = createSlice({
   initialState: INITIAL_STATE,
   reducers: {
     uploadAudio: state => ({ ...state, error: null }),
-    changeAudio: (state, { payload: audioDetails }) => ({
-      ...state,
-      error: null,
-      audioDetails,
-    }),
     failure: (state, { payload: error }) => ({
       ...state,
       error,
@@ -33,23 +18,13 @@ const slice = createSlice({
       ...state,
       prediction,
     }),
-    clearAudio: () => ({
-      ...INITIAL_STATE,
-    }),
   },
 })
 
 export const { reducer } = slice
-export const {
-  uploadAudio,
-  changeAudio,
-  failure,
-  success,
-  clearAudio,
-} = slice.actions
+export const { uploadAudio, failure, success } = slice.actions
 
 export const select = {
   error: state => state.record.error,
-  audioDetails: state => state.record.audioDetails,
   prediction: state => state.record.prediction,
 }
